30382ef7d9389984b9a7b917662565d380a99b36
Michael Huang <michaelyhuang23@gmail.com>
2022-07-02 03:11:51 +0000

    results and testers

---
+++ src/test.sh
@@ -0,0 +1,14 @@
+./abcore -ComShrDecom ../data/trec > trec_seq_1.txt
+./abcore -ComShrDecom ../data/trec > trec_seq_2.txt
+./abcore -ComShrDecom ../data/trec > trec_seq_3.txt
+./abcore -ComShrDecom ../data/epinions > epinions_seq_1.txt
+./abcore -ComShrDecom ../data/epinions > epinions_seq_2.txt
+./abcore -ComShrDecom ../data/epinions > epinions_seq_3.txt
+./abcore -ComShrDecom ../data/livejournal > livejournal_seq_1.txt
+./abcore -ComShrDecom ../data/livejournal > livejournal_seq_2.txt
+./abcore -ComShrDecom ../data/livejournal > livejournal_seq_3.txt
+./abcore -ComShrDecom ../data/trackers > trackers_seq_1.txt
+./abcore -ComShrDecom ../data/trackers > trackers_seq_2.txt
+./abcore -ComShrDecom ../data/trackers > trackers_seq_3.txt
+
+
